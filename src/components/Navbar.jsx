b39220e5b4e6f9df0125a249aca6e0944e3d7a0f
Muhammad Hamza <muhammadhamza14210@gmail.com>
2022-12-27 15:32:15 -0700

    final changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt = "Logo Image" style={{width:'50px'}} />
+            <img src={Logo} alt = "Logo" style={{width:'50px'}} />
         </div>
        
         {/*Menu*/}
