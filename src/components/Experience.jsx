776fde5f7c6f0a0ebf1e8c62747d8f6d534bb036
Muhammad Hamza <muhammadhamza14210@gmail.com>
2023-02-21 21:46:24 -0700

    Added work experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,9 +10,20 @@ const Experience = () => {
                 <p className = 'text-4xl font-bold inline border-b-4 border-red-400'>Experience</p>
             </div>
 
+            <div className='shadow-lg shadow-[#040c16] rounded-[10px] p-3'>
+                <h1 className='pb-1 font-bold text-xl'>Co-Founder</h1>
+                <p className='text-sm italic pb-1'><span className='font-bold'>NotoDigitalChord :</span> Feb 2023 - Present</p>
+                  <ul className='p-4 list-disc'>
+                  <li>Scaling local business through digital marketing.</li>
+                  <li>Use of <b>Google Ads, Facebook Ads, Google Analytics, Instagram Ads</b> to monitor progress and run our campaigns.</li>
+                  </ul>
+            </div>
+
+          <div className='pb-10'></div>
+
             <div className='shadow-lg shadow-[#040c16] rounded-[10px] p-3 '>
                 <h1 className='pb-1 font-bold text-xl'>Risk Analyst Intern</h1>
-                <p className='text-sm italic pb-1'>May 2022 - August 2022</p>
+                <p className='text-sm italic pb-1'><span className='font-bold'>Al Ghurair Energy:</span> May 2022 - August 2022</p>
                   <ul className='p-4 list-disc'>
                   <li>Demonstrated expertise in preparing daily reports, including <b>P/L</b> and <b>VAR</b> analysis, improved <b>20%</b> efficiency.</li>
                   <li>Reviewing daily derivative positions to ascertain free equity and notifying leadership team regarding margin call requirements.</li>
